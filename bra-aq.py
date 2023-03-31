de26b93fa3bca12271ceb71cec2756e8a4d935d2
Amy Win <39141516+AmryAnn@users.noreply.github.com>
2023-03-31 13:53:08 -0500

    Update bra-aq.py

--- bra-aq.py
+++ bra-aq.py
@@ -72,6 +72,12 @@ topic_prox_ambient = "v1/" + username + "/things/" + clientid + "/data/6"
 
 topic_sgp40_voc_index = "v1/" + username + "/things/" + clientid + "/data/7"
 
+with open('aq.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    fields = ['Temp(f)', 'Humidity', 'Pressure(atm)', 'Altitude(f)', 'Proximity(cm)',
+            'AmbientLight', 'VOCindex']
+    values = [tempf, humidity, int(pressure/101300), altitudef, proximity, ambient, voc_index]
+    writer.writerow(fields)
 
 #Loop runs until we force an exit or something breaks
 while True:
@@ -177,16 +183,8 @@ while True:
 
         oled.display()
 
-    #write data to csv file
-with open('aq.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    fields = ['Temp(f)', 'Humidity', 'Pressure(atm)', 'Altitude(f)', 'Proximity(cm)',
-            'AmbientLight', 'VOCindex']
-    values = [tempf, humidity, int(pressure/101300), altitudef, proximity, ambient, voc_index]
-    writer.writerow(fields)
-        if u == 60:
-            with open('aq.csv', 'a', newline='') as f:
-                writer = csv.writer(f)
+        with open('aq.csv', 'a', newline='') as f:
+            writer = csv.writer(f)
             #for value in values:
             writer.writerow(values)
 
